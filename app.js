d3487e215f6842a47fe350e79600afd42ed56ea4
elbitpmoC <bazellMP@gmail.com>
2021-06-23 15:37:23 -0400

    Uses var, let & const on objects, arrays & regular variables.

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-// ALERT
-// alert('Alert works');
+// var avatar = 'PB & J';
+// avatar = 'Steve Madden'
+// console.log(avatar);
 
-// Log to console
-console.log('Hello World');
-console.log(123);
-console.log([1, 2, 3, 4, 5]);
-console.log({ a: 1, b: 2, c: 3 });
-console.table({ a: 1, b: 2, c: 3 });
+// //Initialize var
+// var initialVar;
+// console.log(initialVar); //undefined
+// initialVar = 'Howdy Cow-girl';
+// console.log(initialVar);
 
-// Clear console
-console.clear();
-console.error('Dangit Bobby!');
-console.warn('Swiper NO swiping!');
+// //Multi-word variables
+// var firstname; //lower case
+// var first_name; //Underscore
+// var firstName; //Camel case - USE THIS
+// var FirstName; //PascalCase - Utilize w/ Classes and Constructor functions
 
-// Time
-console.time('Hello');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.timeEnd('Hello');
+// LET
+// let avatar = 'PB & J';
+// avatar = 'Steve Madden'
+// console.log(avatar);
+
+// CONST
+const avatar = 'PB & J';
+// avatar = 'Steve Madden' //We can NOT reasign a value w/ const variables.
+console.log(avatar);
+
+// CONST object variables, the information within can be changed around, same with Arrays.
+const glasses = {
+  sunGlasses: true,
+  waterGlasses: false,
+  readingGlasses: true,
+  lost: true
+}
+glasses.sunGlasses = false;
+console.log(glasses);
