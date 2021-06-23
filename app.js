750b019d0f6c4951733bf5eecacea4589ee3bb7d
elbitpmoC <bazellMP@gmail.com>
2021-06-23 16:08:40 -0400

    Primitive Types & Reference Types

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-// var avatar = 'PB & J';
-// avatar = 'Steve Madden'
-// console.log(avatar);
+// Primitive Data Types
+// --------------------
+const avatar = 'Johnny Applesed'
+console.log(typeof avatar); // String
+console.log(typeof 1)// Number
+console.log(typeof true)// Boolean
+console.log(typeof null)// Null
+console.log(typeof x) //undefined
+console.log(typeof Symbol()) //Symbol
+// --------------------
 
-// //Initialize var
-// var initialVar;
-// console.log(initialVar); //undefined
-// initialVar = 'Howdy Cow-girl';
-// console.log(initialVar);
+// Reference Data Types - These are being accessed from memory AS a reference
+// --------------------
 
-// //Multi-word variables
-// var firstname; //lower case
-// var first_name; //Underscore
-// var firstName; //Camel case - USE THIS
-// var FirstName; //PascalCase - Utilize w/ Classes and Constructor functions
+// Array
+const flavors = [
+  'orange', 'peach', 'cherry', 'raspberry', 'kiwi', 'watermelon'
+]
 
-// LET
-// let avatar = 'PB & J';
-// avatar = 'Steve Madden'
-// console.log(avatar);
-
-// CONST
-const avatar = 'PB & J';
-// avatar = 'Steve Madden' //We can NOT reasign a value w/ const variables.
-console.log(avatar);
-
-// CONST object variables, the information within can be changed around, same with Arrays.
-const glasses = {
-  sunGlasses: true,
-  waterGlasses: false,
-  readingGlasses: true,
-  lost: true
+// Object literal
+const person = {
+  job: 'Web Master',
+  hobbies: [
+    '3D Animation',
+    'Drawing',
+    'Animation',
+    'Motion',
+    'Interactive Design',
+    'Reading',
+    'Guitar',
+    'Piano',
+    'App Dev',
+    'Teaching'
+  ],
+  age: 20,
+  state: 'Washington'
 }
-glasses.sunGlasses = false;
-console.log(glasses);
+
+console.log(typeof person)
+console.log(Date());
+const y = new Date();
+console.log(typeof y);
