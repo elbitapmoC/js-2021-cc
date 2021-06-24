9bba2ac291484b34c3a2cbaf2d85f2f420e53f7b
elbitpmoC <bazellMP@gmail.com>
2021-06-24 18:19:15 -0400

    Utilizes Math methods and basic math functions

--- app.js
+++ app.js
@@ -1,56 +1,27 @@
+const num1 = 100;
+const num2 = 50;
 let val;
 
-// TO STRING
-// ---------
-// Num -> String
-// val = String(555);
-// val = String(4 + 4);
-// To string(method)
-// val = (555).toString();
-// val = (4 + 4).toString();
+// Simple math
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2; // Modulus, returns the remaining number.
 
-// Bool to String
-// val = String(true);
-// To string(method)
-// val = (true).toString();
+// Math Object
+val = Math.PI;
+val = Math.round(2.5); // 3
+val = Math.round(2.4); // 2
+val = Math.ceil(2.1); // 3 - Rounds up to the next largest integer
+val = Math.floor(2.1) // 2 - Rounds down
+val = Math.sqrt(16); // 4
+val = Math.abs(-444); // 444
+val = Math.pow(7, 3) // (7*7*7) = 343
+val = Math.min(948, 54, 63, 5, 7, 878, 54, 57, 23, 24, 5, 4, -1);
+val = Math.max(948, 54, 63, 5, 7, 878, 54, 57, 23, 24, 5, 4, -1);
+val = Math.random(); // Generates a random number from 0 to 1 (for example: 0.2858934643822625)
+val = Math.random() * 20; // Generates a random number from 0 to 19
+val = Math.round(Math.random() * 151); // Generates a random number from 0 to 19
 
-// Date to String
-// val = String(new Date());
-// To string(method)
-// val = (new Date()).toString();
-
-// Array to string
-// val = String(['guava', 'mint', 'baby bananas', 'mango'])
-// To string(method)
-// val = (['guava', 'mint', 'baby bananas', 'mango']).toString();
-
-// Bool to String
-// val = String(true);
-// To string(method)
-// val = (true).toString();
-
-// TO NUMBER
-// ---------
-// val = Number('5');
-// val = Number(true);
-// val = Number(null);
-// val = Number('Aloha') //NaN
-// val = Number(['1', '2', '3'])
-
-// // val = parseInt('123.654');
-// val = parseFloat('28.25')
-
-// Output
-// console.log(val);
-// console.log(typeof val)
-// console.log(val.length)
-// console.log(val.toFixed(2))
-
-const val1 = String('1');
-const val2 = 3;
-let sum = val1 + val2;
-console.log(sum)
-console.log(Number(val1) + val2)
-sum = Number(val1) + val2;
-console.log(typeof sum)
-
+console.log(val)
