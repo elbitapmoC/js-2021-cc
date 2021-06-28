0d0da9c2164bf8ebb1b7366106f47075dcbf5186
elbitpmoC <bazellMP@gmail.com>
2021-06-28 15:15:38 -0400

    Refreshes on if statements, comparisons, and more.

--- app.js
+++ app.js
@@ -15,15 +15,40 @@ const person = {
   }
 }
 
-let val
+let val = '100';
 
-val = person.fN;
-val = person['fN'];
-val = person.hobbies[4]
-val = person.getBirthYear();
+// equal to
+if (val >= 100) console.log("You're spending too much.\nWe need to do better budgeting.");
+else console.log("Good stuff, we're under 100")
 
-for (let i = 0; i < person.hobbies.length; i++) {
-  console.log(person.hobbies[i]);
-}
+// NOT equal to
+if (val != 100) console.log("Bummer!");
+else console.log("Rad dood!")
 
-console.log(val)
+// equal to VALUE & TYPE
+if (val === 100) console.log("You're spending too much.\nWe need to do better budgeting.");
+else console.log("Good stuff, !100")
+
+// NOT equal to VALUE & TYPE
+if (val !== 100) console.log("Bummer!");
+else console.log("Rad dood!")
+
+// test undefined
+if (typeof val === undefined) console.log("Undefined...");
+else console.log("Defined!")
+
+// great than
+if (val > 100) console.log("greater than");
+else if (val < 100) console.log("less than")
+else console.log("Neither..")
+
+// AND
+if (val > 10 && val < 100) console.log('In between AND');
+else console.log('False AND')
+
+// OR
+if (val > 10 || val <= 100) console.log('One is true OR');
+else console.log('False OR')
+
+// TERNARY
+console.log(val === 100 ? 'Spot on!' : 'Nahh...')
