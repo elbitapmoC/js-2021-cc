9931ac00a460f7e85f826973e8315e89d98d76b9
elbitpmoC <bazellMP@gmail.com>
2021-06-28 14:41:05 -0400

    Advances with arrays and array methods.

--- app.js
+++ app.js
@@ -1,72 +1,29 @@
-const fruit = ['orange', 'apple', 'peach'];
-const testNewArray = new Array(1, 2, 3, 4, 5, 6, 10, 20, 30, 40, 50, 100, 60, 63, 44, 102, 11);
-const mixedFruit = [1, 2, true, false, { a: 1, b: 2 }, 'canon', new Date()];
-
-let val;
-
-// Get array length
-// console.log(fruit.length)
-
-// Check if an item is an array
-// console.log(Array.isArray(fruit))
-
-// Get a single value
-val = fruit[2];
-// console.log(val)
-
-// insert into the array
-fruit[0] = 'Mango';
-// console.log(fruit)
-
-// Find index of a value
-// console.log(mixedFruit.indexOf('canon')); // 5
-
-// MUTATING ARRAYS
-// Add to the end 
-fruit.push('Guava')
-// Add to the front
-fruit.unshift('Mamey')
-
-// Take off from end
-fruit.pop()
-// Take off from front
-fruit.shift();
-
-// Splice values - Removes everything from 0 to 4, keeps the 5th element
-// testNewArray.splice(0, 5)
-
-// Reverse
-testNewArray.reverse();
-
-// concat
-val = fruit.concat(testNewArray);
-
-// sort
-// val = testNewArray.sort(); // [1, 10, 100, 102, 11, 2, 20 ...]
-
-// Sorting by utilizing compare function
-// val = testNewArray.sort((x, y) => {
-//   return x - y;
-// });
-// sorting from low to high.
-
-
-// Sorting by utilizing compare function
-// val = testNewArray.sort((x, y) => {
-//   return y - x;
-// });
-// reversing our sort from high to low.
-
-function under50(num) {
-  return num < 50;
+const person = {
+  fN: 'Apple',
+  lN: 'Macintosh',
+  age: 45,
+  email: 'me@apple.com',
+  address: {
+    city: 'Cupertino',
+    state: 'California',
+    country: 'U.S.',
+    locale: '1 Apple Park Way'
+  },
+  hobbies: ['tech', 'cars', 'phones', 'subscriptions', 'networking'],
+  getBirthYear: function () {
+    return 2021 - this.age;
+  }
 }
 
-function over50(num) {
-  return num > 50;
+let val
+
+val = person.fN;
+val = person['fN'];
+val = person.hobbies[4]
+val = person.getBirthYear();
+
+for (let i = 0; i < person.hobbies.length; i++) {
+  console.log(person.hobbies[i]);
 }
 
-
-val = testNewArray.find(under50); // 11
-val = testNewArray.find(over50); // 102
-
-console.log(val);
+console.log(val)
