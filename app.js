ec89b0bca8fd9cb135d7166f8174999537e66e49
elbitpmoC <bazellMP@gmail.com>
2021-06-30 15:47:33 -0400

    Save progress

--- app.js
+++ app.js
@@ -1,66 +1,24 @@
 let val;
+const taskTitle = document.getElementById('task-title');
 
-// What's in the head?
-val = document.head;
+// Getting Things From the Element
+val = taskTitle;
+val = taskTitle.id; // task-title
+// val = document.getElementById('task-title').className; //empty
 
-// What doctype is it?
-val = document.doctype;
+// Change styling of an Element
+taskTitle.style.color = 'lavender';
+taskTitle.style.backgroundColor = 'black';
+taskTitle.style.padding = '5px';
+taskTitle.style.display = 'inline-block';
 
-// Get domain
-val = document.domain;
+// Change content
+taskTitle.textContent = 'Text Content';
+taskTitle.innerText = 'Inner Text..'
+taskTitle.innerHTML = '<h6>innerHTML</h6>'
 
-// Get URL
-val = document.URL;
+// Query Selector
+document.querySelector('li').style.color = 'red'; // Gets the first element (li)
+val = document.querySelector('li'); // Gets the first element (li)
 
-// Get charset
-val = document.charset;
-
-// Get content type
-val = document.contentType;
-
-// Checkout the document
-val = document;
-
-// Checkout all elements within the document
-val = document.all;
-
-// Checkout a specific element within all the document
-val = document.all[5]; // materialize
-
-// Get the length of the elements within the document
-val = document.all.length
-
-// What's in the body?
-val = document.body;
-
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id; //task-form
-val = document.forms[0].method; // GET || POST
-val = document.forms[0].action; // Forms usually have an action, the action will appear here.
-
-val = document.links;
-
-val = document.links[5];
-val = document.links[0].id;
-
-val = document.links[5].className;
-val = document.links[5].classList;
-val = document.links[5].classList[0];
-// val = document.links.length; // 6
-
-val = document.images; // 0
-
-val = document.scripts;
-// val = document.scripts.length;
-// val = document.scripts[2].src;
-// val = document.scripts[2].getAttribute('src');
-
-let scripts = document.scripts; // HTML Collection
-scriptsArr = Array.from(scripts); //Converts HTML Collection to an Array
-// Print out the source for each element within the scriptsArr
-scriptsArr.forEach((script) => {
-  console.log(script.getAttribute('src'));
-});
-
-// console.log(val);
+console.log(val);
