5415bd961d658f50619a62e551960b3cd4b0479f
elbitpmoC <bazellMP@gmail.com>
2021-07-02 15:18:18 -0400

    Updates comments

--- s3-creating-elements.js
+++ s3-creating-elements.js
@@ -1,29 +1,30 @@
-// Create an element
+// Create an element(li)
 const li = document.createElement('li');
 
-// Add a claass
-li.className = 'collection-item'
+// Add a class to above element
+li.className = 'collection-item';
 
-// Add an id
-li.id = 'new-item'
+// Add an id (new-item)
+li.id = 'new-item';
 
-// Add an attribute
-li.setAttribute('title', 'New Item')
+// Add an attribute - ('title', 'New Item')
+li.setAttribute('title', 'new-item');
 
-// Create text node and append
-li.appendChild(document.createTextNode('Hello World'))
+// Create text node and append it to the li
+li.appendChild(document.createTextNode('Helloooo Nurse!'));
 
-// Create a new link element
+// variable(link) to store a createElement (a)
 const link = document.createElement('a');
 
-// Add approprate classes
-link.className = 'delete-item secondary-content';
-// Add icon html
+// Add approprate classes to above link - 'delete-item secondary-content'
+link.className = 'delete-item secondary-content'
+
+// Add html ('<i class="fa fa-remove"></i>') to link
 link.innerHTML = '<i class="fa fa-remove"></i>';
 
-// Append link to li
+// Add link to li
 li.appendChild(link);
 
-// Append li as a child to ul
+// Add li as a child to ul
 document.querySelector('ul.collection').appendChild(li);
 console.log(li)
