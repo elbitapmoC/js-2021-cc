560111656ced36bdc0625172df9145be27eaba39
elbitpmoC <bazellMP@gmail.com>
2021-07-06 10:40:09 -0400

    Updates pathing

---
+++ s3/07-mouse-events.js
@@ -0,0 +1,18 @@
+// Store clear tasks button
+// Store Card
+// Store Tasks heading
+
+// Event Listeners
+// Click
+
+// Double Click
+
+// Mouse Down
+
+// Mouse Up
+
+// Mouse Enter
+
+// Mouse Leave
+
+// Event Handler
