9c3565d69c204c05f59c133271396b113ba4b8b7
elbitpmoC <bazellMP@gmail.com>
2021-07-06 11:18:14 -0400

    Adds mouse event

--- s3/07-mouse-events.js
+++ s3/07-mouse-events.js
@@ -1,18 +1,32 @@
-// Store clear tasks button
-// Store Card
-// Store Tasks heading
+const clearButton = document.querySelector('.clear-tasks');// Store clear tasks button
+const card = document.querySelector('.card');// Store Card
+const heading = document.querySelector('h5');// Store Tasks heading
 
-// Event Listeners
+function runEvent(e) {
+  console.log(`Event Type: ${e.type}`);
+  heading.textContent = `X: ${e.offsetX} Y:${e.offsetY}`;
+}
+
+// -EVENT LISTENERS-
 // Click
+clearButton.addEventListener('click', runEvent);
 
 // Double Click
+clearButton.addEventListener('dblclick', runEvent);
 
 // Mouse Down
+clearButton.addEventListener('mousedown', runEvent);
 
 // Mouse Up
+clearButton.addEventListener('mouseup', runEvent);
 
 // Mouse Enter
+card.addEventListener('mouseenter', runEvent);
 
 // Mouse Leave
+card.addEventListener('mouseleave', runEvent);
 
-// Event Handler
+// Mouse move
+card.addEventListener('mousemove', runEvent);
+
+// -EVENT HANDLER-
