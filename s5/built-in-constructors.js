a1cb5025208d0c199b73a13e55a26b1a509a3d05
elbitpmoC <bazellMP@gmail.com>
2021-07-22 13:26:00 -0400

    save progress

--- s5/built-in-constructors.js
+++ s5/built-in-constructors.js
@@ -1,37 +1,37 @@
-// // STRINGS
-// const name1 = 'Jeff';
-// const name2 = new String('Jeff');
+// STRINGS
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
 
-// name2.foo = 'bar';
+name2.foo = 'bar';
 
-// console.log(typeof name2); // OBJECT
-// console.log(name2); // String {"Jeff"}
-// // 0: "J"
-// // 1: "e"
-// // 2: "f"
-// // 3: "f"
-// // foo: "bar"
-// // length: 4
+console.log(typeof name2); // OBJECT
+console.log(name2); // String {"Jeff"}
+// 0: "J"
+// 1: "e"
+// 2: "f"
+// 3: "f"
+// foo: "bar"
+// length: 4
 
 // // == compares value
-// if (name2 == 'Jeff') console.log('AYEEE');
-// else console.log('BYEEE');
+if (name2 == 'Jeff') console.log('AYEEE');
+else console.log('BYEEE');
 
 // // === comparse type && value.
-// if (name2 === 'Jeff') console.log('2-AYEEE');
-// else console.log('2-BYEEE');
+if (name2 === 'Jeff') console.log('2-AYEEE');
+else console.log('2-BYEEE');
 
 // // NUMBER
-// const num1 = 7;
-// const num2 = new Number(7)
-// console.log(typeof num2); // OBJECT
-// console.log(num2); // Number {7}
+const num1 = 7;
+const num2 = new Number(7)
+console.log(typeof num2); // OBJECT
+console.log(num2); // Number {7}
 
 // BOOLEAN
-// const bool1 = true;
-// const bool2 = new Boolean(true)
-// console.log(typeof bool2);// OBJECT
-// console.log(bool2);// Boolean {true}
+const bool1 = true;
+const bool2 = new Boolean(true)
+console.log(typeof bool2);// OBJECT
+console.log(bool2);// Boolean {true}
 
 // FUNCTION, OBJECT, ARRAYS
 // We won't be using keywords...
