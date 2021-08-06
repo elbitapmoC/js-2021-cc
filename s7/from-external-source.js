0dd7b4d99206b8825d3cce21c0721a165d04c5df
elbitpmoC <bazellMP@gmail.com>
2021-08-06 12:21:41 -0400

    Changes api we use, this one gives us the book order, random scripture, and text.

--- s7/from-external-source.js
+++ s7/from-external-source.js
@@ -1,40 +1,27 @@
-document.getElementById('generate-psalm').addEventListener('click', getPsalm);
+document.getElementById('generate-random-verse').addEventListener('click', getScripture)
+document.addEventListener("DOMContentLoaded", function () {
+  getScripture();
+});
 
-let chapter;
+const scripture = {}
 
-function getPsalm() {
+function getScripture() {
   const data = null;
 
   const xhr = new XMLHttpRequest();
-  // xhr.withCredentials = true;
+  xhr.withCredentials = true;
 
-  // xhr.addEventListener("readystatechange", function () {
-  //   if (this.readyState === this.DONE) {
-  //     console.log(this.responseText);
-  //   }
-  // });
-
-  // REQUEST Type, Url, Asychronus (t/f)
-  xhr.open("GET", `https://ajith-holy-bible.p.rapidapi.com/GetChapter?Book=Psalms&chapter=${getRandomInt(150)}`, true);
-
-  xhr.setRequestHeader("x-rapidapi-key", "4c3fd7e65amsh233a617ec9edde7p178257jsn210a88807cb2");
-  xhr.setRequestHeader("x-rapidapi-host", "ajith-holy-bible.p.rapidapi.com");
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const response = this.responseText;
-      console.log(response);
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      let response = JSON.parse(this.responseText);
+      let minifiedResponse = (response.results[0]);
+      console.log(minifiedResponse);
     }
-  }
+  });
+
+  xhr.open("GET", "https://uncovered-treasure-v1.p.rapidapi.com/random");
+  xhr.setRequestHeader("x-rapidapi-key", "4c3fd7e65amsh233a617ec9edde7p178257jsn210a88807cb2");
+  xhr.setRequestHeader("x-rapidapi-host", "uncovered-treasure-v1.p.rapidapi.com");
 
   xhr.send(data);
 }
-
-function getRandomInt(max) {
-  chapter = Math.floor(Math.random() * max)
-  document.getElementById('psalm-prayer').innerText = `PSA Ch.${chapter}`
-
-  return chapter;
-}
-
-
-
